a876996febea900a59b229085a5d2e5012ba3196
jihua kang <kangjihua@gmail.com>
2016-04-04 00:00:39 +0800

    first commit, simple niuniu tokenizer

---
+++ src/org/wltea/analyzer/lucene/IKTokenizerFactory.java
@@ -0,0 +1,24 @@
+package org.wltea.analyzer.lucene;
+
+import java.io.Reader;
+import java.util.Map;
+
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.util.TokenizerFactory;
+
+public class IKTokenizerFactory extends TokenizerFactory{
+	
+	boolean useSmart;
+	
+	@Override
+  public void init(Map<String, String> args) {
+    super.init(args);
+    useSmart = Boolean.parseBoolean(args.get("useSmart"));
+  }
+
+	@Override
+	public Tokenizer create(Reader in)
+	{
+        return new IKTokenizer(in, useSmart);
+    }
+}
